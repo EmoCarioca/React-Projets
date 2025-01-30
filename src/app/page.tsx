762f21dddcb51d7847d5b315f2607ad3b0281074
EmoCarioca <104680041+EmoCarioca@users.noreply.github.com>
2025-01-30 19:51:36 -0300

    df

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,54 @@
 "use client"
 
-import { Cb } from "@/components/cb";
+import { Tod } from "@/types/todo"
+import { useState } from "react"
 
 
 const pag = () => {
+  const [itemI, setI] = useState('')
+  const [list, setList] = useState<Tod[]>([
+    { label: 'fiz o dever de casa?', check: false },
+    { label: 'lavei louça?', check: false }
 
-  const handleB1 = () => alert('clickou 1')
-  const handleB2 = () => alert('clickou 2')
-  const handleB3 = () => alert('clickou 3')
 
+  ])
+
+  const toggleI = (index: number) => {
+    let nL = [...list]
+    nL[index].check = !nL[index].check
+    setList(nL)
+  }
+
+  const handB = () => {
+    if (itemI.trim() === '') return;
+    setList([...list, { label: itemI, check: false }])
+    setI('')
+
+  }
+  const deleteI = (index: number) => {
+    setList(list.filter((item, key) => key !== index))
+  }
 
   return (
-    <div className='gap-2 container w-screen h-screen justify-center items-center flex'>
-      <Cb label="click h" onClick={handleB1}/>
-      <Cb label="click h2" onClick={handleB2}/>
-      <Cb label="click h3"onClick={handleB3} />
+    <div className="w-screen h-screen flex flex-col items-center">
+      <h1>lista de tarefas</h1>
+      <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-600">
+        <input type="text" className="rounded-md border border-black text-black p-3 mr-3 text-2xl "
+          placeholder="O que deseja fazer?" value={itemI} onChange={e => setI(e.target.value)} />
+        <button onClick={handB}>Adicionar</button>
+      </div>
 
+      <p className="my-4">{list.length} itens na lista</p>
+      <ul className="pl-5 list-disc w-full max-w-lg">
+        {list.map((item, index) => (
+          <li key={index}>
+            <input type="checkbox" checked={item.check} className="p-3" onClick={() => toggleI(index)} />
+            {item.label} - <button className="hover:underline" onClick={() => deleteI(index)}>[deletar]</button></li>
+        ))}
+      </ul>
     </div>
+
+
   )
-
-
-
 }
-
-
 export default pag;
---
+++ src/types/todo.ts
@@ -0,0 +1,4 @@
+export type Tod = {
+    label:string
+    check:boolean
+}
