4dbe71f281510711a1ddf5172fe54b2227e8b764
EmoCarioca <104680041+EmoCarioca@users.noreply.github.com>
2025-03-07 17:59:45 -0300

    unf

---
+++ sc/assets/exerciciosconc/unfinished tasks/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Eg from "@/components/contador"
+import { Task } from "@/components/task"
+import { useReducer } from "react"
+
+export const eg = () => {
+    return (
+        <Task />
+        ///<Eg />
+
+    )
+}
+
+export default eg
