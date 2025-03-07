75cea57ce3a6158c7b18eff417f969dc2495dc29
EmoCarioca <104680041+EmoCarioca@users.noreply.github.com>
2025-03-07 18:00:28 -0300

    unf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 import Eg from "@/components/contador"
-import { Task } from "@/components/task"
 import { useReducer } from "react"
 
 export const eg = () => {
     return (
-        <Task />
         ///<Eg />
 
     )
